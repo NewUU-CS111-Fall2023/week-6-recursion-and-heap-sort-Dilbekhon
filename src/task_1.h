0d9a5b67b552db9b63c39bece90b55da4b5fe6e1
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2023-11-23 18:51:36 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,24 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+void printNumbersRecursive(int a, int b) {
+    if (a == b) {
+        std::cout << a << " ";
+    } else if (a < b) {
+        std::cout << a << " ";
+        printNumbersRecursive(a + 1, b);
+    } else {
+        std::cout << a << " ";
+        printNumbersRecursive(a - 1, b);
+    }
+}
+
+int main() {
+    // Taking input
+    int A, B;
+    std::cin >> A >> B;
+
+    // Printing numbers using recursion
+    printNumbersRecursive(A, B);
+
+    return 0;
+}
